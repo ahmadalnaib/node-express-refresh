976b8d7e8503382d184f131edb9733aa73bef34c
ahmedhabeeb73 <alnaib888@gmail.com>
2025-03-04 16:19:56 +0100

    create static server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 
 app.listen(8000, () => {
